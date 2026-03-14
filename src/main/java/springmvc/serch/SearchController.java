f9a97dd24321449691eea3efbaddc59f49e875d7
Shibu_01 <shibuy0011@gmail.com>
2026-03-14 12:04:05 +0530

    Initial commit

---
+++ src/main/java/springmvc/serch/SearchController.java
@@ -0,0 +1,30 @@
+package springmvc.serch;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SearchController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "redirect:/home";
+	}
+
+	@RequestMapping("/home")
+	public String home() {
+	   return "home";
+	}
+
+	@RequestMapping("/search")
+	public RedirectView search(@RequestParam("query-box") String query) {
+		
+		String url = "https://www.google.com/search?q=" +query;
+		
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl(url);
+		return redirectView;
+	}
+}
