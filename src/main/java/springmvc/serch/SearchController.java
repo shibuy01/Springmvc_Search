dedd7884e974d31b69d29ea0b4ac42a6a0bf9faf
Shibu_01 <shibuy0011@gmail.com>
2026-03-16 01:01:33 +0530

    upload

--- src/main/java/springmvc/serch/SearchController.java
+++ src/main/java/springmvc/serch/SearchController.java
@@ -1,12 +1,19 @@
 package springmvc.serch;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class SearchController {
+	
+	@RequestMapping("/user/{userId}")
+	public String getUserDetails(@PathVariable("userId") int userId) {
+		System.out.println(userId);
+		return "home";
+	}
 	
 	@RequestMapping("/")
 	public String index() {
